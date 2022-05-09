99d5da8a6e0977b82509cd93754d7b073d9ebc80
André <andrelara2002@gmail.com>
2022-05-09 08:15:25 -0300

    Configurações para layout mobile

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,6 +1,6 @@
 export default function Modal({ title, hidden, description, onClick, buttonLabel, className }) {
 
-    return <div className={"modal " + className ? className : ''} hidden={hidden}>
+    return <div className={`modal ${className ? className : ''}`} hidden={hidden}>
         <h1>{title}</h1>
         <h2>{description}</h2>
         <button onClick={()=>{onClick()}}>{buttonLabel}</button>
